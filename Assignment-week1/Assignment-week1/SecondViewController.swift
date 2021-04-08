3bba4085a5727147e616a6450380910e99e9749a
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-04-08 23:30:09 +0900

    feat : 비밀번호 다를때 alert 추가

--- Assignment-week1/Assignment-week1/SecondViewController.swift
+++ Assignment-week1/Assignment-week1/SecondViewController.swift
@@ -22,11 +22,19 @@ class SecondViewController: UIViewController {
 
     @IBAction func joinMemberButtonClicked(_ sender: Any) {
         
+        let alert = UIAlertController(title: "오류!", message: "비밀번호와 비밀번호 확인이 다릅니다. 다시 입력해주세요", preferredStyle: UIAlertController.Style.alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            
+        }
+        alert.addAction(okAction)
+        
         guard let ThirdVC = self.storyboard?.instantiateViewController(identifier: "ThirdViewController")
             as? ThirdViewController else { return}
         
         if emailTextField.text != "", passwordTextField.text != "", passwordOkTextField.text != "" {
-            
+            if passwordTextField.text != passwordOkTextField.text {
+                present(alert, animated: false, completion: nil)
+            }
             ThirdVC.name = emailTextField.text
             
             ThirdVC.modalPresentationStyle = .fullScreen
