5bcdbd0cf4edb89cb05fb16581e1a0ec3b5c5ab1
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-04-10 23:40:57 +0900

    seminar : Week-2 Seminar

---
+++ Seminar/SOPT-seminar-2/SOPT-seminar-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SOPT-seminar-2
+//
+//  Created by 김혜수 on 2021/04/10.
+//  Copyright © 2021 kimhyesu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Seminar/SOPT-seminar-2/SOPT-seminar-2Tests/SOPT_seminar_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  SOPT_seminar_2Tests.swift
+//  SOPT-seminar-2Tests
+//
+//  Created by 김혜수 on 2021/04/10.
+//  Copyright © 2021 kimhyesu. All rights reserved.
+//
+
+import XCTest
+@testable import SOPT_seminar_2
+
+class SOPT_seminar_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
